fd6cf8df1a98c343a5b20289d581f20922015c5e
pdshetley <patriciashetely@gmail.com>
2016-03-16 18:13:47 -0700

    completed some of checkpoint 8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,9 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
           var SongPlayer = {};
+         // private attribute stores album information
+        var currentAlbum = Fixtures.getAlbum();
+        SongPlayer.currentAlbum = currentAlbum;
          
  /**
  * @desc Buzz object audio file
@@ -13,55 +16,100 @@
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
  */
-         var setSong = function(song) {
+         
+        var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                SongPlayer.SongPlayer.currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
- 
+            
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
- 
-            SongPlayer.SongPlayer.SongPlayer.currentSong.playing = song;
-      };
-         
-          /**
- * @desc Active song object from list of songs
- * @type {Object}
- */
-          SongPlayer.SongPlayer.SongPlayer.SongPlayer.currentSong = null;
-    }
-         
-
-          SongPlayer.play = function(song) {
-              if (SongPlayer.SongPlayer.currentSong.playing !== song) {
-                  
-              setSong(song);
-              currentBuzzObject.play();
-              song.playing = true;
-          } else if (SongPlayer.SongPlayer.currentSong.playing === song) {
-              if (currentBuzzOject.isPaused()) {
-                  currentBuzzObject.play();
+            
+            SongPlayer.currentSong = song;
+        }; 
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = false;
+        };
+        
+        // This function will get the index of a song
+        var getSongIndex = function(song) {
+          return currentAlbum.songs.indexOf(song);  
+        };
+        
+        /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+        //This is now a public attribute so we can use it within the player bar.
+        SongPlayer.currentSong = null;
+        
+        SongPlayer.play = function(song) {
+            /* We use || to tell the function: assign (1) the value of song or (2) the value of SongPlayer.currentSong to the song variable. The first condition occurs when we call the methods from the Album view's song rows, and the second condition occurs when we call the methods from the player bar. */
+            song = song || SongPlayer.currentSong;
+            
+            if (song === undefined && song === null) {
+                return;
+            }
+            
+            if (SongPlayer.currentSong !== song) {
+                
+                setSong(song);
+                playSong(song);
+                
+            } else if (SongPlayer.currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
                 }
-          }
-   };
-         
-         SongPlayer.pause = function(song) {
-             currentBuzzObject.pause();
-             song.playing = false;
-         };
-     
- /**
-         var playSong = function()
-         currentBuzzObject.play();
-         song.playing = true;
-     };
-*/
-         
-   return SongPlayer;
-}
+            }
+        };
+        
+        SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
+        
+        SongPlayer.previous = function() {
+          var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            // To test this, I've chosen the current number of songs: 5. Not sure what .length I should be testing here.
+            if (currentSongIndex >= currentAlbum.songs.length) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+            
+        };
+        
+        return SongPlayer;
+    }
  
      angular
          .module('blocJams')
