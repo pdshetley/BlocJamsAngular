64be66f689923a52f0fc15cb026c6678a23a87cf
pdshetley <patriciashetely@gmail.com>
2016-03-22 16:29:44 -0700

    made chagnes during mentor meeting

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -6,37 +6,39 @@
             var offsetXPercent = offsetX / seekBarWidth;
             offsetXPercent = Math.max(0, offsetXPercent);
             offsetXPercent = Math.min(1, offsetXPercent);
-            return offsetXPercent;
-        }
-      };
+            return offsetXPercent
+        };
          
          return {
-         templateUrl: '/templates/directives/seek_bar.html',
-         replace: true,
-         restrict: 'E',
-         scope: { },
-         link: function(scope, element, attributes) {
-             scope.value = 0;
-             scope.max = 100;
-             
-             var seekBar = $(element);
- 
-             var percentString = function () {
-                 var value = scope.value;
-                 var max = scope.max;
-                 var percent = value / max * 100;
-                 return percent + "%";
-             };
- 
-             scope.fillStyle = function() {
-                 return {width: percentString()};
-             };
-             
-             scope.onClickSeekBar = function(event) {
-                var percent = calculatePercent(seekBar, event);
-                scope.value = percent * scope.max;
-             };
-     };
+             templateUrl: '/templates/directives/seek_bar.html',
+             replace: true,
+             restrict: 'E',
+             scope: { },
+             link: function(scope, element, attributes) {
+                 scope.value = 0;
+                 scope.max = 100;
+
+                 var seekBar = $(element);
+
+                 var percentString = function () {
+                     var value = scope.value;
+                     var max = scope.max;
+                     var percent = value / max * 100;
+                     return percent + "%";
+                 };
+
+                 scope.fillStyle = function() {
+                     return {width: percentString()};
+                 };
+
+                 scope.thumbStyle = function() {
+                     return {left: percentString()};
+                 };
+
+                 scope.onClickSeekBar = function(event) {
+                    var percent = calculatePercent(seekBar, event);
+                    scope.value = percent * scope.max;
+                 };
              
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
@@ -51,6 +53,9 @@
                         $document.unbind('mouseup.thumb');
                     });
             };
+        }
+    };
+}
  
      angular
          .module('blocJams')
